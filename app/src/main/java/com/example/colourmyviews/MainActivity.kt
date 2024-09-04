442abbbc2e7ac7dee6ce8c75e819a0ba0b13577d
Sajeewa Malith <sajeewamalith0219@gmail.com>
2024-09-04 14:02:47 +0530

    Add Aligned Boxes with Click Handlers

--- app/src/main/java/com/example/colourmyviews/MainActivity.kt
+++ app/src/main/java/com/example/colourmyviews/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.colourmyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +16,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraint_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val box_one_text = findViewById<TextView>(R.id.box_one_text)
+        val box_two_text = findViewById<TextView>(R.id.box_two_text)
+        val box_three_text = findViewById<TextView>(R.id.box_three_text)
+        val box_four_text = findViewById<TextView>(R.id.box_four_text)
+        val box_five_text = findViewById<TextView>(R.id.box_five_text)
+        val constraint_layout = findViewById<ConstraintLayout>(R.id.constraint_layout)
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text
+            ,box_five_text,box_four_text,constraint_layout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 }
